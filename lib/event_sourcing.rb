9e33eb3850bfa3051b3b033f172c7edc1054bbcc
Oren Mittman <nihil2501@gmail.com>
2023-08-14 11:45:13 -0400

    fix event sourcing

--- lib/event_sourcing.rb
+++ lib/event_sourcing.rb
@@ -1,22 +1,41 @@
 # frozen_string_literal: true
 
 module EventSourcing
+  # TODO: Here we hardcoded domain-specific logic into our generic event
+  # sourcing module. Ideally, this module exposes a way to register
+  # configurations that some other domain-specific code location will leverage.
+  # The configuration should allow one to specify 4 parameters such that an
+  # event will be associated through a topic to the appropiate aggregate.
+  # The configuration parameters are:
+  #   * event_type
+  #   * get_id_from_event
+  #   * aggregate_type
+  #   * get_id_from_aggregate
+  # These parameters would be packaged together under a particular topic family,
+  # and then a topic will be the pairing of this family with an entity id.
   class << self
     def on_event_loaded(event)
-      case event
-      when Rayhub::Events::DeviceReading::Count
-        topic = :"device_reading/#{event.device_id}/count"
-        TopicQueue.enqueue(topic, event)
-      end
+      topic =
+        case event
+        when Rayhub::Events::DeviceReading::Count
+          :"device_reading/#{event.device_id}/count"
+        end
+
+      TopicQueue.enqueue(
+        topic,
+        event
+      )
     end
 
     def on_aggregate_loaded(aggregate)
-      case aggregate
-      when Rayhub::Aggregates::DeviceReading::Count
-        topic = :"device_reading/#{aggregate.device_id}/count"
-        TopicQueue.drain(topic) do |event|
-          aggregate.apply(event)
+      topic =
+        case aggregate
+        when Rayhub::Aggregates::DeviceReading::Count
+          :"device_reading/#{aggregate.device_id}/count"
         end
+
+      TopicQueue.drain(topic) do |event|
+        aggregate.apply(event)
       end
     end
   end
